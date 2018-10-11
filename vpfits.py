d4252d292b6f7c3e4643c6260208d9d1467293b2
Sarah Appleby <sarahappleby20@gmail.com>
2018-10-11 14:15:45 +0100

    indentation

--- vpfits.py
+++ vpfits.py
@@ -69,7 +69,7 @@ class VPfit():
         Args:
             x (numpy array): wavelength array
             centroid (float): center of profile
-f            alpha (float): Gaussian HWHM
+            alpha (float): Gaussian HWHM
             gamma (float): Lorentzian HWHM
         """
 
