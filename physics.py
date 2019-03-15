Fixed b-parameter calculation to use FWHM instead of standard deviation. Also fixed the values stored for 'region_numbers' in the parameters file.

physics.py
@@ -24,7 +24,7 @@ def DopplerParameter(sigma, line):
     """
     # convert line position from Angstroms to m.
     line *= 1.e-10
-    return (line*sigma / np.sqrt(2))*1.e-3
+    return (line*sigma*2.355 / np.sqrt(2))*1.e-3 #multiply by 2.355 to convert from std. dev. to FWHM
 
 def EquivalentWidth(taus, edges):
     """

vpfits.py
@@ -850,7 +850,7 @@ def fit_spectrum(wavelength_array, noise_array, flux_array, line, voigt=False, c
         params['b'] = np.append(params['b'], DopplerParameter(sigmas, line))
         params['N'] = np.append(params['N'], ColumnDensity(heights, sigmas))
         params['centers'] = np.append(params['centers'], centers)
-        params['region_numbers'] = np.append(params['region_numbers'], centers)
+        params['region_numbers'] = np.append(params['region_numbers'], region_numbers)
         for k in range(n):
             params['EW'] = np.append(params['EW'], EquivalentWidth(fit.estimated_profiles[k].value, [waves[0], waves[-1]]))
         