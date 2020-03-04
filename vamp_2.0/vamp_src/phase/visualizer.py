e2e4212c3883675f9e03dc109b055850e4d5fd24
Sarah Appleby <sapple@roe.ac.uk>
2020-03-04 12:01:22 +0000

    added residual plot

--- vamp_2.0/vamp_src/phase/visualizer.py
+++ vamp_2.0/vamp_src/phase/visualizer.py
@@ -7,8 +7,7 @@ class Visualizer(): # do i need the abstract class?
 		self.dataset = dataset
 		self.visualize_path = visualize_path
 
-	def visualize_fit(self, fit, n_components, during_analysis):
-
+	def plot_fit(self, fit, n_components):
 		plt.plot(self.dataset.frequency, self.dataset.flux, c='k', label='Data')
 		plt.plot(self.dataset.frequency, fit, c='b', ls='--', label='fit, %g components' % n_components,)
 		plt.legend()
@@ -17,3 +16,15 @@ class Visualizer(): # do i need the abstract class?
 		plt.savefig(self.visualize_path + 'fit.png')
 		plt.clf()
 
+	def plot_residuals(self, fit):
+		residual = self.dataset.flux - fit
+		plt.plot(self.dataset.frequency, residual, c='b')
+		plt.xlabel('Frequency')
+		plt.ylabel('Residual')
+		plt.savefig(self.visualize_path + 'residual.png')
+		plt.clf()
+
+	def visualize_fit(self, fit, n_components, during_analysis):
+
+		self.plot_fit(fit, n_components)
+		self.plot_residuals(fit)
