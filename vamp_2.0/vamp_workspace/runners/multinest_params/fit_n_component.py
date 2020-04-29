2cf61721d898adc96cdd329ea887d23819cec657
Sarah Appleby <sarahappleby20@gmail.com>
2020-04-29 17:05:59 +0100

    param test with assertions and priors

--- vamp_2.0/vamp_workspace/runners/multinest_params/fit_n_component.py
+++ vamp_2.0/vamp_workspace/runners/multinest_params/fit_n_component.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('/disk2/sapple/VAMP/vamp_2.0')
 from vamp_src.model import profile_models
 from vamp_src.dataset.spectrum import *
+from vamp_src.dataset.preprocess import *
 import vamp_src.phase.phase as ph
 
 workspace_path = "{}/../../".format(os.path.dirname(os.path.realpath(__file__)))
@@ -14,54 +15,68 @@ af.conf.instance = af.conf.Config(
     config_path=config_path, output_path=workspace_path + "output_params"
 )
 
+ncomp_true = '2'
 param_setting = 'default'
-combo = 'e'
-ncomp = 2
+combos = ['a', 'b', 'c', 'd', 'e']
+ncomps = [1, 2, 3]
 spectrum_dir = '/home/sarah/VAMP/vamp_2.0/vamp_workspace/runners/multinest_params/data/'
-filename = spectrum_dir+'combo_' + combo + '_1_component.h5'
 
-if ncomp == 1:
-	phase = ph.Phase(
-    	phase_name="phase_x1_combo_"+combo+'_'+param_setting,
-    	profiles=af.CollectionPriorModel(
-        	gaussian_0=profile_models.Gaussian
-    	),
-	)
-elif ncomp == 2:
-	phase = ph.Phase(
-    	phase_name="phase_x2_combo_"+combo+'_'+param_setting,
-    	profiles=af.CollectionPriorModel(
-        	gaussian_0=profile_models.Gaussian, gaussian_1=profile_models.Gaussian
-    	),
-	)
-elif ncomp == 3:
-	phase = ph.Phase(
- 	   phase_name="phase_x3_combo_"+combo+'_'+param_setting,
-    	profiles=af.CollectionPriorModel(
-        	gaussian_0=profile_models.Gaussian, gaussian_1=profile_models.Gaussian, gaussian_2=profile_models.Gaussian
-    	),
-	)
-dataset = read_from_h5py(filename)
-result = phase.run(dataset=dataset)
+chi_squared = np.zeros(10)
+max_log_l = np.zeros(10)
+evidence = np.zeros(10)
 
-plt.plot(dataset.frequency, result.most_likely_model_spectrum)
-plt.plot(dataset.frequency, dataset.flux)
-plt.show()
+i = 0
+for combo in combos:
+    filename = spectrum_dir+'combo_' + combo + '_'+ncomp_true+'_component.h5'
 
-result = phase.run(dataset=dataset)
-model = result.most_likely_model_spectrum
+    print('\nCombo '+combo+'\n')
 
+    for ncomp in ncomps:
 
-print('\nBayesian evidence: ')
-print(result.output.evidence)
-print('\nMax Log Likelihood')
-print(result.output.maximum_log_likelihood)
-print('\nReduced chi squared')
-print(result.analysis.get_reduced_chi_squared(model))
+        print('\nNo components: '+str(ncomp)+'\n')
 
-mp_instance = result.output.most_probable_instance
+        full_dataset = read_from_h5py(filename)
 
-print("\nMost Probable Model:\n")
-print("Centre = ", [i.center for i in mp_instance.profiles])
-print("Intensity = ", [i.intensity for i in mp_instance.profiles])
-print("Sigma = ", [i.sigma for i in mp_instance.profiles])
+        # Lets actually narrow this down by only fitting the absorption region
+        split = SplitRegions(full_dataset)
+        dataset = split.new_region_spectra()
+
+        phase_name="combo_"+combo+"_true_"+ncomp_true+"_phase_"+str(ncomp)+"_"+param_setting
+        sigma_max = 0.5 * (np.max(dataset.frequency) - np.min(dataset.frequency))
+
+        if ncomp == 1:
+            profiles = af.CollectionPriorModel(
+                        gaussian_0=profile_models.Gaussian)
+
+        elif ncomp == 2:
+            profiles=af.CollectionPriorModel(
+                        gaussian_0=profile_models.Gaussian, gaussian_1=profile_models.Gaussian)
+        
+        elif ncomp == 3:
+            profiles = af.CollectionPriorModel(
+                        gaussian_0=profile_models.Gaussian, gaussian_1=profile_models.Gaussian, gaussian_2=profile_models.Gaussian)
+
+        for component in profiles.dict:
+            profiles.dict[component].sigma = af.UniformPrior(lower_limit=0, upper_limit=sigma_max)
+            profiles.dict[component].center = af.UniformPrior(lower_limit=np.min(dataset.frequency), upper_limit=np.max(dataset.frequency)) # might need to adjust this as real frequencies scale inversely
+
+        for n in range(len(profiles) -1):
+            profiles.add_assertion(profiles[n].center < profiles[n+1].center)
+
+        phase = ph.Phase(phase_name=phase_name,profiles=profiles)
+
+        result = phase.run(dataset=dataset)
+
+        model = result.most_likely_model_spectrum
+        chi_squared[i] = result.analysis.get_reduced_chi_squared(model)
+        max_log_l[i] = result.output.maximum_log_likelihood
+        evidence[i] = result.output.evidence
+
+        i += 1
+
+print('\nAll chi squareds (a1, a2, b1, b2...)')
+print(chi_squared)
+print('\nAll max likelihood')
+print(max_log_l)
+print('\nAll evidence')
+print(evidence)
